fdb29443cedb655df188a1e6bbb51b34566ace30
egonz279 <egonz279@calpoly.edu>
2024-03-15 11:02:21 -0700

    done11

--- squareplus1.c
+++ squareplus1.c
@@ -3,60 +3,61 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+
 int main() {
-    int p1[2], p2[2], p3[2];
-    int v;
+    int pipe1[2], pipe2[2], pipe3[2];
+    int value;
 
-    if (pipe(p1) < 0 || pipe(p2) < 0 || pipe(p3) < 0) {
-        perror("Pipe creation failed");
+    if (pipe(pipe1) < 0 || pipe(pipe2) < 0 || pipe(pipe3) < 0) {
+        perror("failed");
         exit(1);
     }
 
-    pid_t c1 = fork();
-    if (c1 < 0) {
-        perror("Fork 1 failed");
+    pid_t child1 = fork();
+    if (child1 < 0) {
+        perror("failed");
         exit(1);
-    } else if (c1 == 0) {
-        close(p1[1]); close(p2[0]); close(p3[0]); close(p3[1]);
+    } else if (child1 == 0) {
+        close(pipe1[1]); close(pipe2[0]); close(pipe3[0]); close(pipe3[1]);
 
-        while (read(p1[0], &v, sizeof(v)) > 0) {
-            v = v * v;
-            write(p2[1], &v, sizeof(v));
+        while (read(pipe1[0], &value, sizeof(value)) > 0) {
+            value = value * value;
+            write(pipe2[1], &value, sizeof(value));
         }
 
-        close(p1[0]); close(p2[1]);
+        close(pipe1[0]); close(pipe2[1]);
         exit(0);
     }
 
-    pid_t c2 = fork();
-    if (c2 < 0) {
-        perror("Fork 2 failed");
+    pid_t child2 = fork();
+    if (child2 < 0) {
+        perror("failed");
         exit(1);
-    } else if (c2 == 0) {
-        close(p1[0]); close(p1[1]); close(p2[1]); close(p3[0]);
+    } else if (child2 == 0) {
+        close(pipe1[0]); close(pipe1[1]); close(pipe2[1]); close(pipe3[0]);
 
-        while (read(p2[0], &v, sizeof(v)) > 0) {
-            v = v + 1;
-            write(p3[1], &v, sizeof(v));
+        while (read(pipe2[0], &value, sizeof(value)) > 0) {
+            value = value + 1;
+            write(pipe3[1], &value, sizeof(value));
         }
 
-        close(p2[0]); close(p3[1]);
+        close(pipe2[0]); close(pipe3[1]);
         exit(0);
     }
 
-    close(p1[0]); close(p2[0]); close(p2[1]); close(p3[1]);
+    close(pipe1[0]); close(pipe2[0]); close(pipe2[1]); close(pipe3[1]);
 
-    while (scanf("%d", &v) != EOF) {
-        write(p1[1], &v, sizeof(v));
-        read(p3[0], &v, sizeof(v));
-        printf("Result: %d\n", v);
+    while (scanf("%d", &value) != EOF) {
+        write(pipe1[1], &value, sizeof(value));
+        read(pipe3[0], &value, sizeof(value));
+        printf("Result: %d\n", value);
     }
 
-    close(p1[1]); close(p3[0]);
+    close(pipe1[1]); close(pipe3[0]);
 
     int status;
-    waitpid(c1, &status, 0);
-    waitpid(c2, &status, 0);
+    waitpid(child1, &status, 0);
+    waitpid(child2, &status, 0);
 
     return 0;
 }
